aacb6f84be361864f76a28f434d2039f4dbed756
Nicolas Mattiocco <nicolas@greenlock.fr>
2020-05-20 16:02:47 +0200

    Initial files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PatrowlHears4py
-Python client for PatrowlHears
+Python API Client for PatrowlHears4py
+
+# Deployment commands
+rm -rf dist/ build/ PatrowlHears4py.egg-info
+python setup.py sdist bdist_wheel
+twine upload dist/*
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Setup file."""
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='PatrowlHears4py',
+    version='1.0.0',
+    description='Python API client for PatrowlHears',
+    url='https://github.com/Patrowl/PatrowlHears4py',
+    author='Nicolas Mattiocco',
+    author_email='getsupport@patrowl.io',
+    license='AGPLv3',
+    packages=find_packages(),
+)
